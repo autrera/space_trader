ca887bf0f3efb5390bccf4272f4e5d9dd64ef9eb
Aldo Utrera <accounts@aldoutrera.com>
2025-04-10 12:34:04 -0500

    Add simple logic to generate a 100x100 grid and a random starting point

--- app.js
+++ app.js
@@ -6,13 +6,40 @@ import Drawer from "./drawer.js";
 import { isLocalStorageAvailable } from "./utilities.js";
 
 export default function App() {
+  const [loaded, setLoaded] = createSignal(false);
+  const universe = [];
+  const startingPoint = {
+    x: Math.floor(Math.random() * 100),
+    y: Math.floor(Math.random() * 100),
+  };
+
   onMount(async () => {
-    const database = await openDB();
-    setDb(database);
+    // const database = await openDB();
+    // setDb(database);
+
+    for (let i = 0; i < 100; i++) {
+      universe[i] = [];
+      for (let j = 0; j < 100; j++) {
+        universe[i].push({
+          name: `Sector: ${i}-${j}`,
+          x: i,
+          y: j,
+        });
+      }
+    }
 
     if (isLocalStorageAvailable()) {
     }
+
+    setLoaded(true);
   });
 
-  return html``;
+  return () => {
+    if (loaded()) {
+      return html`
+        <p>Your are in sector ${startingPoint["x"]}-${startingPoint["y"]}</p>
+        <p>${universe[startingPoint["x"]][startingPoint["y"]].name}</p>
+      `;
+    }
+  };
 }
